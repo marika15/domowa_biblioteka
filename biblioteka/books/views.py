905e0093d2fc462e722fddef38021ae3057eb165
marika15 <marika.kuczynska15@op.pl>
2025-01-17 22:27:46 +0100

    Add category support

--- biblioteka/books/views.py
+++ biblioteka/books/views.py
@@ -1,2 +1,25 @@
 from django.shortcuts import render, redirect, get_object_or_404
  
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'books/category_list.html', {'categories': categories})
+
+def add_category(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        Category.objects.create(name=name)
+        return redirect('category_list')
+    return render(request, 'books/add_category.html')
+
+def edit_category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    if request.method == 'POST':
+        category.name = request.POST.get('name')
+        category.save()
+        return redirect('category_list')
+    return render(request, 'books/edit_category.html', {'category': category})
+
+def delete_category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    category.delete()
+    return redirect('category_list')
